aeab8eb4b6ca8b01b92d59b09a3ddf59324764e1
Edwin Diaz <edwinram.col@gmail.com>
2020-03-11 13:15:21 -0500

    Comentarios en Do While

--- Reto 100 numero naturales/main.c
+++ Reto 100 numero naturales/main.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 
 int main()
-{
+{ 
+    // imprimir por pantalla lo que necitamos
     printf("Reto imprimir 100 numero naturales \n\n");
 
+    // Declaramos una variable de un numero entero
     int numero = 0;
 
+    //Iniciamos el cilco do while 
     do{
        numero++;
         if (numero%2 == 0)
